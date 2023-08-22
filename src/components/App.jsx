c14cd87cd5c915cbb33cb3ed20b7de2ed1d9d5f6
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-08-23 00:05:15 +0300

    React-хуки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,64 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactList } from './ContactList/ContactList';
 import { GlobalStyle, Section } from './GlobalStyle';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  componentDidMount() {
-    console.log('componentDidMount');
-    const SavedContacts = localStorage.getItem('contacts');
-    if (SavedContacts !== null) {
-      console.log(SavedContacts);
-      this.setState({ contacts: JSON.parse(SavedContacts) });
+export function App() {
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState(() => {
+    if (localStorage.getItem('contacts') !== null) {
+      return JSON.parse(localStorage.getItem('contacts'));
+    } else {
+      return [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ];
     }
-  }
-  changeNameFilter = newName => {
-    this.setState({ filter: newName });
+  });
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const changeNameFilter = newName => {
+    setFilter(newName);
   };
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-  handleDelete = quizId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(quiz => quiz.id !== quizId),
-      };
-    });
+
+  const handleDelete = quizId => {
+    setContacts(prevcontacts =>
+      prevcontacts.filter(quiz => quiz.id !== quizId)
+    );
   };
-  addQuiz = newQuiz => {
-    const isContactExists = this.state.contacts.find(
+
+  const addQuiz = newContacts => {
+    const isContactExists = contacts.find(
       contact =>
-        contact.name.toLowerCase() === newQuiz.name.toLowerCase() ||
-        contact.number === newQuiz.number
+        contact.name.toLowerCase() === newContacts.name.toLowerCase() ||
+        contact.number === newContacts.number
     );
     if (isContactExists) {
-      return alert(`${newQuiz.name} is already in contacts`);
+      return alert(`${newContacts.name} is already in contacts`);
     }
-    this.setState(prevState => {
-      return {
-        contacts: [...prevState.contacts, newQuiz],
-      };
-    });
+    setContacts(prevContacts => [...prevContacts, newContacts]);
   };
 
-  render() {
-    const { filter, contacts } = this.state;
-    const visibleQuizItems = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-    return (
-      <>
-        <Section>
-          <h1>Phonebook</h1>
-          <ContactForm onAdd={this.addQuiz} />
+  const visibleQuizItems = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
-          <h2>Contacts</h2>
-          <Filter
-            value={this.state.filter.name}
-            onChange={this.changeNameFilter}
-          />
-          <ContactList
-            contacts={visibleQuizItems}
-            onDelete={this.handleDelete}
-          />
-        </Section>
-        <GlobalStyle />
-      </>
-    );
-  }
+  return (
+    <>
+      <Section>
+        <h1>Phonebook</h1>
+        <ContactForm onAdd={addQuiz} />
+
+        <h2>Contacts</h2>
+        <Filter value={filter.name} onChange={changeNameFilter} />
+        <ContactList contacts={visibleQuizItems} onDelete={handleDelete} />
+      </Section>
+      <GlobalStyle />
+    </>
+  );
 }
