51e16bc99daa5ba3fe5a47eed8d8d71864e06aa6
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-08-23 19:03:55 +0300

    contactsLocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,20 +3,24 @@ import { ContactList } from './ContactList/ContactList';
 import { GlobalStyle, Section } from './GlobalStyle';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
+
+function contactsLocalStorage() {
+  const savedContacts = localStorage.getItem('contacts');
+  if (savedContacts !== null) {
+    return JSON.parse(savedContacts);
+  } else {
+    return [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
+  }
+}
+
 export function App() {
   const [filter, setFilter] = useState('');
-  const [contacts, setContacts] = useState(() => {
-    if (localStorage.getItem('contacts') !== null) {
-      return JSON.parse(localStorage.getItem('contacts'));
-    } else {
-      return [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ];
-    }
-  });
+  const [contacts, setContacts] = useState(contactsLocalStorage);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
@@ -27,8 +31,8 @@ export function App() {
   };
 
   const handleDelete = quizId => {
-    setContacts(prevcontacts =>
-      prevcontacts.filter(quiz => quiz.id !== quizId)
+    setContacts(prevContacts =>
+      prevContacts.filter(quiz => quiz.id !== quizId)
     );
   };
 
